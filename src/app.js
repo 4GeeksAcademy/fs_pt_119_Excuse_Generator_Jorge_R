79cc4fbe9e5091946fc2f13705b6963b5a1d0d94
JorgeCKR <jorgeckr@gmail.com>
2025-08-12 15:12:38 +0000

    Funcion commit

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
 import "bootstrap";
 import "./style.css";
-
-
-
-window.onload = function() {
-  //write your code here
   
+function excusa() {
+
 let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
 let action = ['ate ', 'peed ', 'crushed ', 'broke '];
 let what = ['my homework ', 'my phone ', 'the car '];
 let when = ['before the class ', 'when I was sleeping ', 'while I was exercising ', 'during my lunch ', 'while I was praying '];
 
-let whorandom = who[Math.floor(Math.random() * who.length)]
-let actionrandom = action[Math.floor(Math.random() * action.length)]
-let whatrandom = what[Math.floor(Math.random() * what.length)]
-let whenrandom = when[Math.floor(Math.random() * when.length)]
+let whorandom = Math.floor(Math.random() * who.length);
+let actionrandom = Math.floor(Math.random() * action.length);
+let whatrandom = Math.floor(Math.random() * what.length);
+let whenrandom = Math.floor(Math.random() * when.length);
 
-let randomwords = whorandom + actionrandom + whatrandom + whenrandom
+let  randomword = who[whorandom] +  action[actionrandom] + what[whatrandom] + when[whenrandom];
+return(randomword)
+};
 
-document.getElementById("excuse").innerHTML = randomwords
+let randomword = excusa();
 
+window.onload = function() {
+  document.getElementById("excuse").innerText = randomword
 };
